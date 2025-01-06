18d2c3552bbb6abb5b964e3218b0d58a312ded7e
miffycute <araya.jangiiz@gmail.com>
2025-01-06 19:43:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function openForm() {
+    document.getElementById("myFormlogin").style.display = "block";
+    document.querySelector('.backdrop').style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myFormlogin").style.display = "none";
+    document.querySelector('.backdrop').style.display = "none";
+  }
