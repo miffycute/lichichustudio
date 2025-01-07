9273c76a54748f276c3a27b83ae30d5c6babe713
miffycute <araya.jangiiz@gmail.com>
2025-01-07 16:45:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 
 
 function openForm() {
-    document.getElementById("myFormlogin").style.display = "block";
-    document.querySelector('.backdrop').style.display = "block";
+  document.getElementById("myFormlogin").style.display = "block";
+  document.querySelector('.backdrop').style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myFormlogin").style.display = "none";
+  document.querySelector('.backdrop').style.display = "none";
+}
+
+
+
+function togglePassword() {
+  const passwordField = document.getElementById("searchtext2");
+  const toggleButton = document.querySelector(".btn-outline-secondary i");
+
+  if (passwordField.type === "password") {
+    passwordField.type = "text";
+    toggleButton.classList.remove("fa-eye-slash");
+    toggleButton.classList.add("fa-eye");
+  } else {
+    passwordField.type = "password";
+    toggleButton.classList.remove("fa-eye");
+    toggleButton.classList.add("fa-eye-slash");
   }
-  
-  function closeForm() {
-    document.getElementById("myFormlogin").style.display = "none";
-    document.querySelector('.backdrop').style.display = "none";
-  }
+}
